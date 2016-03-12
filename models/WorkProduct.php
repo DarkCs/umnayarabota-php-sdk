517ba2a2ffcaa82b3c9a5e29de80e75f4da47413
Dark_Cs <darkcs2@gmail.com>
2016-03-13 00:18:34 +0300

    work product links

--- models/WorkProduct.php
+++ models/WorkProduct.php
@@ -18,6 +18,8 @@ class WorkProduct
     public $is_image_ready = 1;
     public $is_video_ready = 1;
 
+    public $links = [];
+
     public function __construct(Product $product, $config = [])
     {
         $this->product = $product;
